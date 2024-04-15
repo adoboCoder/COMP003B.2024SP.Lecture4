61d2d3a7e753e202673f772711170972f97ce282
Jonathan Cruz <jcruz@adobo.dev>
2024-04-15 03:15:49 -0700

    Add project files.

---
+++ COMP003B.2024SP.Lecture4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B._2024SP.Lecture4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ COMP003B.2024SP.Lecture4/ViewModels/ContactViewModel.cs
@@ -0,0 +1,18 @@
+﻿// TODO: add data annotations
+using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B._2024SP.Lecture4.ViewModels
+{
+    public class ContactViewModel
+    {
+        // TODO: add the appropriate properties and data annotations
+        [Required, StringLength(100), Display(Name = "Full Name")]
+        public string Name { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required, Phone]
+        public string Phone { get; set; }
+        [Required, Range(0, 120)]
+        public int Age { get; set; }
+    }
+}
